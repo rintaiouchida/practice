45565449fcdaf3ec69878926ea5f51ccfe8063a3
rintaro uchida <v795b3eed7qzaE05SE@softbank.ne.jp>
2021-03-14 02:01:23 +0900

    add

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md" style="background-color:red;">
-                    practice
+                    Practice
                 </div>
 
                 <div class="links">
